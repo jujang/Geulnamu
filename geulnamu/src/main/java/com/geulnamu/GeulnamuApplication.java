53a07353695c69db93335c1d903377f4367fbe9d
jujang <jeongookjang@naver.com>
2025-05-18 22:43:32 +0900

    chore: project default setting

---
+++ geulnamu/src/main/java/com/geulnamu/GeulnamuApplication.java
@@ -0,0 +1,13 @@
+package com.geulnamu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeulnamuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeulnamuApplication.class, args);
+	}
+
+}
