44873fe4a3f9b1e07c92004071e3794836bebde9
jujang <jeongookjang@naver.com>
2025-08-07 00:52:17 +0900

    feat(be): 토론 조 관련 API 수정 - 토론조 구성 인원 없을 경우에도 초기화를 위해 유효성 검사 통과하도록 수정

--- geulnamu_backend/src/main/java/com/geulnamu/controller/attendance/dto/request/AssignDiscussionGroupsRequest.java
+++ geulnamu_backend/src/main/java/com/geulnamu/controller/attendance/dto/request/AssignDiscussionGroupsRequest.java
@@ -1,7 +1,6 @@
 package com.geulnamu.controller.attendance.dto.request;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,7 +10,6 @@ import java.util.List;
 @AllArgsConstructor
 public class AssignDiscussionGroupsRequest {
 
-    @NotEmpty(message = "토론 그룹 정보는 필수입니다.")
     List<@Valid DiscussionGroupRequest> groups;
 
 }
