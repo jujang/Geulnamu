7b7cfb22f8500391513dca476be0bfd8639aa88f
jujang <jeongookjang@naver.com>
2025-12-03 15:56:25 +0900

    feat(be): 불필요 코드 정리

--- geulnamu_backend/src/main/java/com/geulnamu/repository/meeting/MeetingQueryRepository.java
+++ geulnamu_backend/src/main/java/com/geulnamu/repository/meeting/MeetingQueryRepository.java
@@ -3,9 +3,5 @@ package com.geulnamu.repository.meeting;
 import com.geulnamu.domain.meeting.Meeting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 public interface MeetingQueryRepository extends JpaRepository<Meeting, Long>, MeetingQueryRepositoryCustom {
-    List<Meeting> findByDiscussionTime(LocalDateTime time);
 }
